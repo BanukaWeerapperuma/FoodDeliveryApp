d6e2c45d204a13b77594590eaeb77b39614e0fa7
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2025-02-23 12:26:11 +0530

    create header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order your </h2>
+            <h2>Order your favorite food from here</h2>
+            <p>Choose from a wide range of food options and get them delivered to your doorstep at your convenience.</p>
+            <button>View Menu</button>
         </div>
     </div>
   )
