b60007a0291dc6928ea48080ce52b7421df78884
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2025-02-27 17:06:42 +0530

    Make itemStore and filter

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/frontend_assets/assets'
-import { useState } from 'react'
+import { StoreContext } from '../../context/StoreContext'
 
 const FoodItem = ({id ,name , price ,description, image}) => {
 
-    const [itemCount, setItemCount] = useState(0);
+    const {cartItems , addToCart , removeFromCart} =useContext(StoreContext)
 
   return (
     <div className='food-item' >
         <div className="food-item-img-container">
             <img className='food-item-image' src={image} alt="food-items" />
-            {
-                !itemCount
-                ?<img src={assets.add_icon_white} alt="add_icon_white" />
+            {!cartItems[id]
+                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="add_icon_white" />
                 :   <div className='food-item-counter'>
-
+                        <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
                     </div>
             }
         </div>
