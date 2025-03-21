9398a5a1b4cf311a8390ac12a1a1db01e2f41fcf
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2025-03-22 03:43:11 +0530

    connect mongoDB atles

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://banukaweerapperuma:nYrnjEqwMzSKymam@cluster0.cvd6b.mongodb.net/food-delivery');
+        console.log('DB Connected');
+    } catch (error) {
+        console.error('DB Connection Failed:', error.message);
+    }
+};
---
+++ backend/readme.md
@@ -0,0 +1,8 @@
+
+
+banukaweerapperuma
+nYrnjEqwMzSKymam
+
+mongodb+srv://banukaweerapperuma:nYrnjEqwMzSKymam@cluster0.cvd6b.mongodb.net/?
+
+retryWrites=true&w=majority&appName=Cluster0
