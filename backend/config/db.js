109dabd577f99f4c0cbd34e5619ee983cf545f64
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2025-03-25 01:49:31 +0530

    backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 export const connectDB = async () => {
     try {
         await mongoose.connect('mongodb+srv://banukaweerapperuma:nYrnjEqwMzSKymam@cluster0.cvd6b.mongodb.net/food-delivery');
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+
+
+
 
 
 
@@ -13,8 +17,14 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 
+
 //db config / connection
 connectDB();
+
+//api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images", express.static("uploads"))
+
 
 app.get("/" , (req , res)=>{
         res.send("API Working")
