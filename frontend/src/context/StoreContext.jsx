22efc469592205b053742bbd699f02e3b77a3574
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2025-04-10 02:18:06 +0530

    solve refresh cart Data clear error

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -58,6 +58,12 @@ const StoreContextProvider = (props) => {
     }
 
 
+    const loadCartData = async (token) => {
+            const response = await axios.post(url + "/api/cart/get" ,{}, {headers:{token}})
+            setCartItems(response.data.cartData);
+            
+    }
+
 
     useEffect(() => {
         
@@ -65,6 +71,7 @@ const StoreContextProvider = (props) => {
             await fetchFoodList();
             if(localStorage.getItem("token")) {
                 setToken(localStorage.getItem("token"));
+                await loadCartData(localStorage.getItem("token"));
             }
             
         }
